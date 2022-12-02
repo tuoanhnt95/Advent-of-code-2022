bc042620a2cbd316c9dfac127b76966ce663c042
Oanh Nguyen <myspamvn@yahoo.com>
2022-12-03 00:57:38 +0900

    refactor: Change puzzle two day 2 names

--- day02.rb
+++ day02.rb
@@ -1,20 +1,25 @@
-# a rock, b paper, c scissor
-# x rock 1p, y paper 2p, z scissor 3p
-# lose 0, draw 3, win 6
-
 file = File.read('rock-paper-scissors.txt')
 array = file.split("\n")
 
+# a rock, b paper, c scissors
+# lose 0, draw 3, win 6
+
+## -- First puzzle -- ##
+# X rock 1p, X paper 2p, Z scissor 3p
+
+## -- Second puzzle -- ##
+# X lose, Y draw, Z win
+
 round_rules = [
-  ["rock","rock",3],
-  ["rock","paper",6],
-  ["rock","scissors",0],
-  ["paper","rock",0],
-  ["paper","paper",3],
-  ["paper","scissors",6],
-  ["scissors","rock",6],
-  ["scissors","paper",0],
-  ["scissors","scissors",3]
+  ["A","rock",3],
+  ["A","paper",6],
+  ["A","scissors",0],
+  ["B","rock",0],
+  ["B","paper",3],
+  ["B","scissors",6],
+  ["C","rock",6],
+  ["C","paper",0],
+  ["C","scissors",3]
 ]
 
 shape_rules = [
@@ -27,29 +32,7 @@ shape_symbols = [
   ["X", 'rock'],
   ["Y", "paper"],
   ["Z", "scissors"],
-  ["A", "rock"],
-  ["B", "paper"],
-  ["C", "scissors"]
 ]
-
-score = 0
-
-array.each do |group|
-  symbol_one = shape_symbols.find { |symbol| symbol[0] == group[0] }[1]
-  symbol_two = shape_symbols.find { |symbol| symbol[0] == group[2] }[1]
-  round_rule = round_rules.find { |rule| rule[0] == symbol_one && rule[1] == symbol_two }
-  shape_rule = shape_rules.find { |rule| rule[0] == symbol_two }
-  score += (round_rule[2] + shape_rule[1])
-end
-
-p score
-
-## -- Second round -- ##
-
-# X lose, Y draw, Z win
-# figure out the shape
-# calculate the point
-score_two = 0
 
 round_scores = [
   ["X", 0],
@@ -57,14 +40,21 @@ round_scores = [
   ["Z", 6]
 ]
 
+score = 0
+score_two = 0
+
 array.each do |group|
-  p group
-  symbol_one = shape_symbols.find { |symbol| symbol[0] == group[0] }[1]
-  round_score = round_scores.find { |score| score[0] == group[2] }[1]
-  shape = round_rules.find { |rule| rule[2] == round_score && rule[0] == symbol_one }[1]
-  p shape
-  shape_rule = shape_rules.find { |rule| rule[0] == shape }
-  score_two += (round_score + shape_rule[1])
+  # first puzzle
+  shape = shape_symbols.find { |symbol| symbol[0] == group[2] }[1]
+  round_score = round_rules.find { |rule| rule[0] == group[0] && rule[1] == shape }[2]
+  shape_score = shape_rules.find { |rule| rule[0] == shape }[1]
+  score += (round_score + shape_score)
+  # second puzzle
+  round_score_two = round_scores.find { |score| score[0] == group[2] }[1]
+  shape_two = round_rules.find { |rule| rule[2] == round_score_two && rule[0] == group[0] }[1]
+  shape_score_two = shape_rules.find { |rule| rule[0] == shape_two }[1]
+  score_two += (round_score_two + shape_score_two)
 end
 
+p score
 p score_two
