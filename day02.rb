ea87c5ba6ffd567426feef477f980ef83d1cdad9
Oanh Nguyen <myspamvn@yahoo.com>
2022-12-03 00:09:19 +0900

    feat: Use separate each loop to solve second puzzle day 2

--- day02.rb
+++ day02.rb
@@ -4,27 +4,67 @@
 
 file = File.read('rock-paper-scissors.txt')
 array = file.split("\n")
-p array[-1][2]
 
-score_rules = [
-  ["A","X",1,3],
-  ["A","Y",2,6],
-  ["A","Z",3,0],
-  ["B","X",1,0],
-  ["B","Y",2,3],
-  ["B","Z",3,6],
-  ["C","X",1,6],
-  ["C","Y",2,0],
-  ["C","Z",3,3],
+round_rules = [
+  ["rock","rock",3],
+  ["rock","paper",6],
+  ["rock","scissors",0],
+  ["paper","rock",0],
+  ["paper","paper",3],
+  ["paper","scissors",6],
+  ["scissors","rock",6],
+  ["scissors","paper",0],
+  ["scissors","scissors",3]
 ]
+
+shape_rules = [
+  ["rock",1],
+  ["paper",2],
+  ["scissors",3]
+]
+
+shape_symbols = [
+  ["X", 'rock'],
+  ["Y", "paper"],
+  ["Z", "scissors"],
+  ["A", "rock"],
+  ["B", "paper"],
+  ["C", "scissors"]
+]
+
 score = 0
 
-array.map do |group|
-  # run through score_rule
-  rule = score_rules.find { |rule| group[0] == rule[0] && group[2] == rule[1] }
-  score += (rule[2] + rule[3])
+array.each do |group|
+  symbol_one = shape_symbols.find { |symbol| symbol[0] == group[0] }[1]
+  symbol_two = shape_symbols.find { |symbol| symbol[0] == group[2] }[1]
+  round_rule = round_rules.find { |rule| rule[0] == symbol_one && rule[1] == symbol_two }
+  shape_rule = shape_rules.find { |rule| rule[0] == symbol_two }
+  score += (round_rule[2] + shape_rule[1])
 end
 
 p score
 
+## -- Second round -- ##
 
+# X lose, Y draw, Z win
+# figure out the shape
+# calculate the point
+score_two = 0
+
+round_scores = [
+  ["X", 0],
+  ["Y", 3],
+  ["Z", 6]
+]
+
+array.each do |group|
+  p group
+  symbol_one = shape_symbols.find { |symbol| symbol[0] == group[0] }[1]
+  round_score = round_scores.find { |score| score[0] == group[2] }[1]
+  shape = round_rules.find { |rule| rule[2] == round_score && rule[0] == symbol_one }[1]
+  p shape
+  shape_rule = shape_rules.find { |rule| rule[0] == shape }
+  score_two += (round_score + shape_rule[1])
+end
+
+p score_two
